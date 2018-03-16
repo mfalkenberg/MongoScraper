3739385c4b039b4a51cef3b07562dd5466001b07
Maggie Falkenberg <mf1306@yahoo.com>
2018-03-15 18:37:52 -0700

    server.js update for heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/MongoScraper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_6f61rphn:crldol04857jrt2idu83gced59@ds215709.mlab.com:15709/heroku_6f61rphn";
 mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
